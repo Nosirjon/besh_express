6ac6a63864c5fe6a77ef7a2e1ad51534742248b0
Nosirjon Khakimov <51654326+Nosirjon@users.noreply.github.com>
2024-08-30 11:02:11 +0500

    aa

--- main.py
+++ main.py
@@ -431,18 +431,15 @@ def location (message):
         markup.add(submit)
         bot.send_message(message.chat.id, text=f'Bosh menyu', reply_markup=markup)
 
-
-
-# def send_message():
-#         for i in get_all_chat_id():
-#             of = i[0]
-#             bot.send_photo(f'{of}', photo=open('1.jpg','rb'))
-#             bot.send_message(f'{of}', text='Assalomu Aleykum azizlar, muqaddas Juma ayyomi muborak bo\'lsin!')        
+def send_message():
+        for i in get_all_chat_id():
+            of = i[0]
+            bot.send_photo(f'{of}', photo=open('1.jpg','rb'))
+            bot.send_message(f'{of}', text='Assalomu Aleykum azizlar, muqaddas Juma ayyomi muborak bo\'lsin!')        
 
 if __name__ =='__main__': 
-    
-    # schedule.every().friday.at('09:00').do(send_message)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
-    bot.polling(non_stop=True)
+    schedule.every().friday.at('09:00').do(send_message)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+    # bot.polling(non_stop=True)
